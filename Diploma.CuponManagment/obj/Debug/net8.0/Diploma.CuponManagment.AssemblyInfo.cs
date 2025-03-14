46741b3bccce19ef318ff7cd3f81e936cd35a415
rx6aih <mrsaintsteam@mail.ru>
2025-03-14 16:32:09 +0300

    Other assembly things been added

--- Diploma.CuponManagment/obj/Debug/net8.0/Diploma.CuponManagment.AssemblyInfo.cs
+++ Diploma.CuponManagment/obj/Debug/net8.0/Diploma.CuponManagment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diploma.CuponManagment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+927b3641af3ca611ee2b0100e417130ebcef01df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da2998602c0a436f9e7cbce541dae6da68a3fcaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diploma.CuponManagment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diploma.CuponManagment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Diploma.DAL/obj/Debug/net8.0/Diploma.DAL.AssemblyInfo.cs
+++ Diploma.DAL/obj/Debug/net8.0/Diploma.DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diploma.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+927b3641af3ca611ee2b0100e417130ebcef01df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da2998602c0a436f9e7cbce541dae6da68a3fcaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diploma.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diploma.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
