ecb1520458cbc61f2059b46b542bed1464232bbc
Frolkin.yu <Frolkin.yu@dobrozaim.ru>
2025-05-05 11:04:12 +0300

    heroui fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import {heroui} from "@heroui/react";
 
 export default defineConfig({
     content: [
@@ -13,7 +11,6 @@ export default defineConfig({
     darkMode: "class",
   plugins: [
       react(),
-      tailwindcss(),
-      heroui
+
   ],
 })
