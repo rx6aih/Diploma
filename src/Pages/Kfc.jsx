a27a9466b4ce2b8b6cb24a5ffd548a7d1f8fc3f6
Frolkin.yu <Frolkin.yu@dobrozaim.ru>
2025-05-05 15:11:23 +0300

    pages

---
+++ src/Pages/Kfc.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Kfc = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Kfc;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {HeroUIProvider} from "@heroui/system";
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-    <HeroUIProvider>
-        <App />
-    </HeroUIProvider>
+    <BrowserRouter>
+        <HeroUIProvider>
+            <App />
+        </HeroUIProvider>
+    </BrowserRouter>
 )
